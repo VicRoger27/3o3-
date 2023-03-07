4d63cc697bcb387a3a9a00f9be81d58e73438141
VicRoger27 <121492850+VicRoger27@users.noreply.github.com>
2023-03-07 19:48:15 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(494, music.beat(BeatFraction.Whole))
+music.playTone(523, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(349, music.beat(BeatFraction.Whole))
+music.playTone(330, music.beat(BeatFraction.Whole))
+music.playTone(294, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Double))
+basic.forever(function () {
+	
+})
